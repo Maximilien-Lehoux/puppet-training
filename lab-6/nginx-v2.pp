d2c10a5c9625a8798223f5aa579786aed699568a
Maximilien-Lehoux <maximilien.lehoux@gmail.com>
2021-08-27 08:31:16 +0000

    training installation nginx

---
+++ lab-6/nginx-v2.pp
@@ -0,0 +1,20 @@
+package { 'nginx':
+  ensure => 'present',
+}
+
+service { 'nginx':
+  ensure    => 'running',
+  enable    => 'true',
+  #nginx redemarre que si exécution de package ou de file
+  subscribe => [
+    Package['nginx'],
+    File['/usr/share/nginx/html/index.html'],
+  ],
+}
+
+#eventuelement file notifie le service nginx,
+file { '/usr/share/nginx/html/index.html':
+  content => "<h2>Welcome to nginx-v2.pp By Maximilien on puppet server</h2>",
+  notify => Service['nginx'],
+}
+
