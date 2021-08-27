07529188796293b9580ba961a93e1a41d2081c59
Maximilien-Lehoux <maximilien.lehoux@gmail.com>
2021-08-27 09:08:53 +0000

    modif v3 pour attester les variables

--- lab-7/nginx-v3.pp
+++ lab-7/nginx-v3.pp
@@ -1,23 +1,25 @@
-$app= "nginx"
-$version= "v3"
-$content= "Welcome to $app-$version.pp By Maximilien on puppet server"
+$app = "nginx"
+$version = "v3"
+$content = "<h2>Welcome to ${app}-${version}.pp By Dirane on puppet server</h2>"
 
-package { $app:
-  ensure => 'present',
+package { "install $app":
+  name   => $app,
+  ensure => 'installed',
 }
 
-service { $app:
+service { "start $app":
+  name      => $app,
   ensure    => 'running',
   enable    => 'true',
   #nginx redemarre que si exécution de package ou de file
   subscribe => [
-    Package[$app],
+    Package["install $app"],
     File['/usr/share/nginx/html/index.html'],
   ],
 }
 
 #eventuelement file notifie le service nginx,
 file { '/usr/share/nginx/html/index.html':
-  content => "$content",
-  notify => Service[$app],
+  content => $content,
+  notify  => Service["start $app"],
 }
